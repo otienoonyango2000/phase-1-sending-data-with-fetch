c812373b8a43ab1f9de535f95e2f3896b759a06e
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-12-11 21:53:28 +0300

    sending data with fetchcompleted

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+//  document.addEventListener('DOMContentLoaded', fetch)
+ let name = "George"
+ let email = "otieno.onyango@student.moringschool.com"   
+
+ function submitData(name, email)  {   
+ return fetch(`http://localhost:3000/users`, {
+     method: "POST",
+     headers: {
+         "Content-Type": "application/json",
+         "Accept": "application/json"
+     },
+     body: JSON.stringify( {
+         name,
+         email
+     } )
+
+ })
+
+.then( response =>  response.json())
+.then( object => document.body.innerHTML = object ["id"] )
+.catch( error => document.body.innerHTML = error.message )
+}
+
+submitData(name, email)
